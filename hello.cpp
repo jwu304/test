a30f65e2c721398d5d89fab054e396d5d250960d
Justin Wu <jwu304@ti-01.cs.ucr.edu>
2019-10-10 09:30:44 -0700

    change made

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,14 @@
 //hello world
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
